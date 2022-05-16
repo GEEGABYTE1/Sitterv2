841cc92f81b3e576df5440ac9a9b7fb0b7da0712
Jaival Patel <pateljaival05@gmail.com>
2022-05-16 00:29:08 -0400

    stuff

--- token_deploy/scripts/1_deploy_token.py
+++ token_deploy/scripts/1_deploy_token.py
@@ -2,9 +2,9 @@ from brownie import accounts, config, TokenERC20
 
 initial_supply = 100 * 10 ** 18
 token_name = 'Flipper'
-token_symbol = 'flip'
+token_symbol = 'FLP'
 
 
 def main():
     account = accounts[0]
-    erc20 = TokenERC20.deploy()
+    erc20 = TokenERC20.deploy (initial_supply, token_name, token_symbol, {'from': account})
