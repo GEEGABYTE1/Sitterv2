d97c4f85054884a3006090e0996ab7eaaea192a0
Jaival Patel <pateljaival05@gmail.com>
2022-05-16 10:14:50 -0400

    creating supplier account

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 const Web3 = require('web3')
 var web3 = new Web3(Web3.givenProvider || 'HTTP://127.0.0.1:7545');
 
-web3.eth.getAccounts(console.log);
-// console.log(web3.eth.accounts)
+
+
+async function accounts () {
+    try {
+        const accounts_lst = await web3.eth.getAccounts()
+        return accounts_lst
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+
+async function running () {
+    let lst_of_accounts = await accounts()
+    let supply_account = lst_of_accounts[0]
+    console.log(supply_account)
+
+}
+
+
+running()
