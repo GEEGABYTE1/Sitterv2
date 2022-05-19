2281b2d5b9e4f5d38f6c4a19c4bb9e363184806f
Jaival Patel <pateljaival05@gmail.com>
2022-05-19 18:51:45 -0400

    fetch everthing news

--- news.js
+++ news.js
@@ -1,6 +1,20 @@
 
 const api_key = 'dbf3d5894a304a3b8c2861d160f1a63c'
 const NewsAPI = require('newsapi')
-const newsapi = new NewsAPI('dbf3d5894a304a3b8c2861d160f1a63c');
+const newsapi = new NewsAPI(api_key);
 
 
+const topHeadLines = (cat) => {
+    newsapi.v2.everything({
+        q: cat,
+        sources: 'bbc-news,the-verge',
+        domains: 'bbc.co.uk, techcrunch.com', 
+        from: '2017-12-01',
+        to: '2021-01-01',
+        language: 'en',
+        sortBy: 'relevancy',
+        page: 2
+      }).then(response => {
+        console.log(response);
+      });
+}
